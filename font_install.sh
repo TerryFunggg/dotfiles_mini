cfcc9403611e88c88b2669944929aa4fb0ebc93f
TerryFunggg <terryyessfung@gmail.com>
2024-12-22 11:02:58 +0800

    fontinstall: add nerd symbols fonts

--- font_install.sh
+++ font_install.sh
@@ -8,7 +8,9 @@ cd $cache_dir
 
 ## Install fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/Hack.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/NerdFontsSymbolsOnly.zip
 unzip Hack.zip
+unzip NerdFontsSymbolsOnly.zip
 
 if [ ! -d "$font_dir" ]; then
 	mkdir $font_dir
